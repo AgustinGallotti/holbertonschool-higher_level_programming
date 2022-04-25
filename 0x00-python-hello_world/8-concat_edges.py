95e2f939671a8acb9acc9f70ce6c462374cd4625
AgustinGallotti <Agustingallotti23@gmail.com>
2022-04-25 15:01:33 -0700

    replace

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[39:67] + str[107:111], end = " ")
-print(str[:6])
+str[107:111].replace("\n", "") 
+print(str[39:67] + str[107:111] + ' ' + str[0:6])
