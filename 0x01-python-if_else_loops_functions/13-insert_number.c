fdd9459a437b84c9ba7ae2a5376dfe17b90cf68a
AgustinGallotti <Agustingallotti23@gmail.com>
2022-04-27 06:43:06 -0700

    talking

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,10 @@
+listint_t *insert_node(listint_t **head, int number)
+//allocate node
+{
+	struct Node* new = (struct Node*) malloc (sizeof(struct Node))
+}
+//out in the data
+{
+	new->data = new_data
+
+}
