f6c0f108d233ececd43d348bbdc69360b69f26fd
AgustinGallotti <Agustingallotti23@gmail.com>
2022-04-26 17:19:27 -0700

    correct format

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,2 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+def print_last_digit(number):
 
+    if (number > 0):
+        r = number % 10
+    else:
+        r = (number * -1) % 10
+    print(f"{r}", end='')
+    return(r)
