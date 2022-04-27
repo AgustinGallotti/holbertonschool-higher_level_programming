e3ccd7d30fd3f4100764799ad6cc761f56ba5a90
AgustinGallotti <Agustingallotti23@gmail.com>
2022-04-26 17:00:33 -0700

    mayus error

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,8 +8,8 @@ else:
     n = number % -10
 
 if n > 5:
-    print(f"The last digit of {number} is {n} and is greater than 5")
+    print(f"Last digit of {number} is {n} and is greater than 5")
 elif n == 0:
-    print(f"The last digit of {number} is {n} and is 0")
+    print(f"Last digit of {number} is {n} and is 0")
 else:
-    print(f"The last digit of {number} is {n} and is less than 6 and not 0")
+    print(f"Last digit of {number} is {n} and is less than 6 and not 0")
