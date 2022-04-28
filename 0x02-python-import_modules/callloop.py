cc0f5fe2f987b132bd0a084b8a8061f5f0979f94
AgustinGallotti <Agustingallotti23@gmail.com>
2022-04-28 15:10:29 -0700

    name main

--- 0x02-python-import_modules/callloop.py
+++ 0x02-python-import_modules/callloop.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for alphabet in range(ord('A'), ord('Z') + 1):
-    print(chr(alphabet), end="")
-print("")
+if __name__ == '__main__':
+    for alphabet in range(ord('A'), ord('Z') + 1):
+        print(chr(alphabet), end="")
+    print("")
