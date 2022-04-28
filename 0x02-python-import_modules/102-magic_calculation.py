d339bc4edffb1e54a94b15ac260f5e0db47faa06
AgustinGallotti <Agustingallotti23@gmail.com>
2022-04-28 16:01:33 -0700

    add with ,

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -4,7 +4,6 @@ def magic_calculation(a, b):
     if a < b:
         c = a + b
         for i in range(4, 6):
-            c = c + i
+            c = add(c, i)
         return c
-    else:
-        return(a - b)
+    return(sub(a, b))
