1b4bef4bb13e72a237cf8ed60ae58720ba155770
AgustinGallotti <Agustingallotti23@gmail.com>
2022-04-27 19:45:20 -0700

    number for add because number is int

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,10 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+* listint_t - we list
+*
+* Return: a new value in the list
+*/
 listint_t *insert_node(listint_t **head, int number)
-//allocate node
 {
-	struct Node* new = (struct Node*) malloc (sizeof(struct Node))
-}
-//out in the data
-{
-	new->data = new_data
+	listint_t *current, *add;
 
+	add = malloc(sizeof(listint_t));
+	if (!add)
+		return (NULL);
+	add->n = number;
+	current = *head;
+	if (*head == NULL || (*head)->n) {
+		add->next = *head;
+		(*head) = add;
+		return (*head);
+	}
+	else {
+		current = *head;
+		while (current->next != NULL && current->next->n 
+			< add->n) {
+			current = current->next;
+		}
+		add->next = current->next;
+		current->next = add;
+		}
+	return (*head);
 }
