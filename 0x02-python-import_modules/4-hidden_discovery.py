520bbc6a92cee9a25daeeaa1bfa9dcb2ef515c46
AgustinGallotti <Agustingallotti23@gmail.com>
2022-04-28 13:03:16 -0700

    commit for no comments

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-#ignore in compilation
 if __name__ == '__main__':
     import sys
     import hidden_4
-    #access to directory
     for i in dir(hidden_4):
-        #__hidden__ exapmle of before [:2] = "__"
         if i[:2] != "__":
             print(i)
