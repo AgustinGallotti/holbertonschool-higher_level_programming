e419f4e33ed60ebeab67002a8f9e60b96df0a51c
AgustinGallotti <Agustingallotti23@gmail.com>
2022-04-28 14:08:11 -0700

    Float functional

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
-if __name__ = '__main__'
-    for i in 
+if __name__ == "__main__":
+    import sys
+    arguments = len(sys.argv)
+    adding = 0
+
+    for i in range(1, arguments):
+        adding += int(sys.argv[i])
+    print(f"{adding}")
+        
