3f46bf358083edb6d85630f94967efb137b91e10
AgustinGallotti <Agustingallotti23@gmail.com>
2022-05-02 12:11:36 -0700

    check with method items()

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in matrix:
-        for j in i:
-            print(j, end=" " if j != i[-1] else "")
+    for item in matrix:
+        for j in item:
+            print(j, end=" " if j != item[-1] else "")
         print("$")
