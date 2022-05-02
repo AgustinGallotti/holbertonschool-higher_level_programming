d3867ab0f25c243883f22d641bd300b1710185b0
AgustinGallotti <Agustingallotti23@gmail.com>
2022-05-02 08:25:46 -0700

    chec

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx > 0: #no se puede imprimir
-        if idx <= len(my_list): #puede recorrerlo
-            return my_list[idx] #returna una posicion x en la lista
+    if idx > len(my_list):
+        return None
+    elif idx < 0:
+        return None
+    else:
+        return my_list[idx]
