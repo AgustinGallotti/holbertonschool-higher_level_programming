d99add134ce3df23351c76aed6a4444455ae3eb4
AgustinGallotti <Agustingallotti23@gmail.com>
2022-05-02 15:39:22 -0700

    errors of two

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    for idx, element in enumerate(my_list):
-        if element == 4:
-            my_list[idx] = 9
+    if idx < 0:
+        return (my_list)
+    elif idx >= len(my_list):
+        return (my_list)
+    else:
+        for idx, element in enumerate(my_list):
+            if element == 4:
+                my_list[idx] = 9
 
     return (my_list)
