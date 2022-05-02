0813792c2d6cfce148ed85e256f50f5d696781e2
AgustinGallotti <Agustingallotti23@gmail.com>
2022-05-02 15:36:18 -0700

    errors

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx > len(my_list):
-        return None
+    if idx >= len(my_list):
+        return
     elif idx < 0:
-        return None
+        return
     else:
         return my_list[idx]
---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+* is_palidrome - is plaidrome
+*
+* return: if else
+*/
+int is_palidrome(listint_t **head)
+{
+	if (!head)
+		return 1;
+}
