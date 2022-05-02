4ee69b617ce080e837d049eba508136150a3c2d0
AgustinGallotti <Agustingallotti23@gmail.com>
2022-05-02 14:24:01 -0700

    checks

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 a = 89
 b = 10
-
-print("a={:d} - b={:d}".format(a, b))
+if a:
+    if b:
+        a, b = b, a
+        print("a={:d} - b={:d}".format(a, b))
+else:
+    print("error in match")
