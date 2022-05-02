a32df3c346e1827b67a5359d70800b17749ff426
AgustinGallotti <Agustingallotti23@gmail.com>
2022-05-02 14:26:38 -0700

    cheacks twelve

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 a = 89
 b = 10
-if a:
-    if b:
-        a, b = b, a
-        print("a={:d} - b={:d}".format(a, b))
-else:
-    print("error in match")
+a, b = b, a
+print("a={:d} - b={:d}".format(a, b))
