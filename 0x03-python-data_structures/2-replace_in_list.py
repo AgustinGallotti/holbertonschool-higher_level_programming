0c31b21d1ecbae9083f79745bdd1a4ad42be106e
AgustinGallotti <Agustingallotti23@gmail.com>
2022-05-02 08:27:03 -0700

    chec

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     for idx, element in enumerate(my_list):
-        if element == 4: #or "== my_list[idx:x]
+        if element == 4:
             my_list[idx] = 9
 
     print(my_list)
