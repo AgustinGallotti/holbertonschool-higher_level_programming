1d009d591bfbad27147dd8b7a8a16413240703d3
AgustinGallotti <Agustingallotti23@gmail.com>
2022-05-02 15:50:45 -0700

    cp2 new

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx > 0:
-        if idx <= len(my_list): #si se cumple, podemos recorrer
-            cp_list = list(my_list) #creamos un copia de seguridad
-            cp_list[idx] = element #agregamos el nuevo elemento
-            return cp_list
+    cp2 = list(my_list)
+    if idx < 0:
+        return cp2
+    elif idx >= len(my_list):
+        return cp2
+    else:
+        cp_list = list(my_list)
+        cp_list[idx] = element
+        return cp_list
