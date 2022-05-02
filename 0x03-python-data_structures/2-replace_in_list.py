db07b5dc0af70b97bac39ed551f0378da00e08fc
AgustinGallotti <Agustingallotti23@gmail.com>
2022-05-02 15:43:23 -0700

    output correct

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -5,7 +5,6 @@ def replace_in_list(my_list, idx, element):
     elif idx >= len(my_list):
         return (my_list)
     else:
-        for idx, element in enumerate(my_list):
-            my_list[idx] = element
+        my_list[idx] = element
 
         return (my_list)
