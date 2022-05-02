a9d9ca5e0ebfe1d34f564a47c9cc92ee8ed9804a
AgustinGallotti <Agustingallotti23@gmail.com>
2022-05-02 09:11:58 -0700

    task to repair

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if tuple < 2:
+        return 0
+    if tuple > 2:
+        res = tuple(map(lambda i, j: i + j, tuple_a, tuple_b))
+        print(res)
