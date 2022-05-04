07b0e6ec2fa3c93ce961fd36235ff83c60f3cf2b
AgustinGallotti <Agustingallotti23@gmail.com>
2022-05-04 10:04:36 -0700

    space in pritn

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def simple_delete(a_dictionary, key=""):
+    for i in a_dictionary
+        key = i.pop("")
+        if key != None:
+            print("True")
+        else:
+            print("The dictionary won't change")
+    print(new)
