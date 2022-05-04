84f9807396d26e3b3d2ee35d914c184949c57ba2
AgustinGallotti <Agustingallotti23@gmail.com>
2022-05-04 12:57:47 -0700

    border to task one

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new = (my_list.copy())
-    if my_list:
-        for search, item in enumerate(my_list):
-            if item == search:
-                new[search] = replace
-        return new
+    for search, item in enumerate(my_list):
+        if item == search:
+            new[search] = replace
+    return new
