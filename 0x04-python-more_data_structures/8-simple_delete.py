5cb81895af020688390af19955568595719ecae6
AgustinGallotti <Agustingallotti23@gmail.com>
2022-05-04 10:16:07 -0700

    task 8 with none c is fun solved

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    for i in a_dictionary
-        key = i.pop("")
-        if key != None:
-            print("True")
-        else:
-            print("The dictionary won't change")
-    print(new)
+    a_dictionary.pop(key, None)
+    return (a_dictionary)
