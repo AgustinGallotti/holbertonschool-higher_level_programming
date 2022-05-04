03ba5e669e141d36a900282758b6e9e7fa46100e
AgustinGallotti <Agustingallotti23@gmail.com>
2022-05-04 14:28:28 -0700

    none new bu yes new

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    new_dict = (a_dictionary.copy())
-    if a_dictionary[key]:
-        new_dict[key] = value
-    else:
-        return new_dict
+    a_dictionary[key] = value
+    return a_dictionary
