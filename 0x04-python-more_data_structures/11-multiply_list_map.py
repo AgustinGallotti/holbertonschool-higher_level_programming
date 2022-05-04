f26df0ae63248d87c501577955a432736e76f8a0
AgustinGallotti <Agustingallotti23@gmail.com>
2022-05-04 14:54:21 -0700

    zip for mult

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
     new_list = (my_list.copy())
-    new_list = [x*number for x, nubmer in zip(my_list, new_list)]
+    new_list = list(map(lambda number,y: number*y, my_list, new_list))
     return new_list
