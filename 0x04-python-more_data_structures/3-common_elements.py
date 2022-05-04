91406c8766dd0ff44c1593d6567e05b3a1745381
AgustinGallotti <Agustingallotti23@gmail.com>
2022-05-04 13:46:54 -0700

    intersection with form of x

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    for element in set_1:
-        if element in set_2:
-            return list(element)
-
+    c = list(set(set_1).intersection(set_2))
+    return c
