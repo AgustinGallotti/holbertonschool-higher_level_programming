6aef8bddba66cacd8833ef05e0a5dacd2ba3d532
AgustinGallotti <Agustingallotti23@gmail.com>
2022-05-04 19:25:44 -0700

    average with /

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,6 +2,8 @@
 def weight_average(my_list=[]):
     val1 = 0
     val = 0
+    if my_list == None:
+        break:
     for value, weight in my_list:
         val += value * weight
         val1 += weight
