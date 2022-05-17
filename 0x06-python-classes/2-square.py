860111d84182e137f4ca42cf81e2eca8112959ef
AgustinGallotti <Agustingallotti23@gmail.com>
2022-05-17 15:49:42 -0700

    task three without size in define

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 class Square:
     __size = None #private class of my size
-    try type(size) == int:
-        def __init__(self, size=0): #new class
-        self.__size=size
-    except type(size) != int:
-            print(TypeError, "size must be an integer\n")
-    except (size < 0):
-        print(TypeError, "size must be >= 0\n")
+    def __init__(self, size=0): #new class
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size=size
---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+class Square:
+    """type of class private"""
+    __size = None
+    """define my class"""
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            """finally realize my task"""
+            self.__size=size
+    """ defien other module """
+    def area(self):
+        """define my function in only one line, without self"""
+        return self.__size ** 2
+        """return my function in pow of two"""
