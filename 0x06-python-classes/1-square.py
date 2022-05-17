b1aa9cadb003e5a2b96c278f6b2700827e0738cd
AgustinGallotti <Agustingallotti23@gmail.com>
2022-05-17 15:53:29 -0700

    task one with comments fixed

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 class Square:
-    __size = None #private class of my size
-    def __init__(self, size): #new class
-        self.__size=size #instance attribute of size
+    """private class"""
+    __size = None
+    """new class"""
+    def __init__(self, size):
+        """instance of my class"""
+        self.__size=size
