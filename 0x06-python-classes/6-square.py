1c0dde3ba9bd39fae9e938c312b00d51d3ddf02e
AgustinGallotti <Agustingallotti23@gmail.com>
2022-05-17 18:25:25 -0700

    one return fixed

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -41,8 +41,7 @@ class Square:
         if self.__size == 0:
             print("")
         else:
-            if self.__position[1] > 0:
-                for i in range(self.__position[1]):
-                    print("")
+            for i in range(self.__position[1]):
+                print("")
             for j in range(self.__size):
                 print(" " * self.__position[0] + "#" * self.__size)
