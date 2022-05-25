3a85238f4bf645d21c60ef6e39955e2bf48319a3
AgustinGallotti <Agustingallotti23@gmail.com>
2022-05-24 17:09:44 -0700

    debbuginishingin

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -7,50 +7,49 @@ max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
     """define my class"""
+    def test_normal(self):
+        """check normal case"""
+        self.assertEqual(max_integer([1, 2, 3 ,4]), 4)
+        self.assertEqual(max_integer([[-1, -2], [1, 2]]), [1, 2])
 
     def test_positive(self):
-    """no duplicates"""
-    list = [1, 2, 3, 0, 5]
-    self.assertEqual(max_integer(list), 5)
+        """no duplicates"""
+        list = [1, 2, 3, 0, 5]
+        self.assertEqual(max_integer(list), 5)
 
     def duplicate(self):
-    """test a list with duplicate non-max values"""
-    list = [2, -7, 3, 2, -7]
-    self.assertEqual(max_integer(list), 3)
+        """test a list with duplicate non-max values"""
+        list = [2, -7, 3, 2, -7]
+        self.assertEqual(max_integer(list), 3)
 
     def duplicate_max(self):
-    list = [2, -7, 3, 3, -7]
-    self.assertEqual(max_integer(list), 3)
-
-    def test_negative_result(self):
-    """negative numbers largest number"""
-    list = [-1, -2, 0, -6, -3]
-    self.assertEqual(max_integer(list), -1)
+        list = [2, -7, 3, 3, -7]
+        self.assertEqual(max_integer(list), 3)
 
     def test_negative(self):
-    """test last element max"""
-    list = [-1, -3, -5, -4, -6]
-    self.assertEqual(max_integer(list), -1)
+        """test last element max"""
+        list = [-1, -3, -5, -4, -6]
+        self.assertEqual(max_integer(list), -1)
 
     def test_first(self):
-    list = [10, 5, -6, 3, 2]
-    self.assertEqual(max_integer(list), 10)
+        list = [10, 5, -6, 3, 2]
+        self.assertEqual(max_integer(list), 10)
 
     def test_all_0(self):
-    list = [0, 0, 0, 0, 0]
-    self.assertEqual(max_integer(list), 0)
+        list = [0, 0, 0, 0, 0]
+        self.assertEqual(max_integer(list), 0)
 
     def test_empty(self):
-    list = []
-    self.assertEqual(max_integer(list), None)
+        """check if is empty"""
+        self.assertEqual(max_integer([]), None)
+        self.assertEqual(max_integer(), None)
 
     def test_all_1(self):
-    list = [1, 1, 1, 1, 1]
-    self.assertEqual(max_integer(list), 1)
+        list = [1, 1, 1, 1, 1]
+        self.assertEqual(max_integer(list), 1)
 
     def only_element(self):
-    list = [1]
-    self.assertEqual(max_integer(list), 1)
+        self.assertEqual(max_integer([1]), 1)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
