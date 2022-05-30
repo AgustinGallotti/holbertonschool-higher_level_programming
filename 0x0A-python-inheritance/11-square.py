3afabae8da6d39afab75ebd42e1e482576e865db
AgustinGallotti <Agustingallotti23@gmail.com>
2022-05-30 15:36:56 -0700

    accept f

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""module"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """square defined"""
+
+    def __init__(self, size):
+        """initialize the size"""
+        super().__init__(size, size)
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """defined area"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """print"""
+        return (f"[Square] {self.__size}/{self.__size}")
