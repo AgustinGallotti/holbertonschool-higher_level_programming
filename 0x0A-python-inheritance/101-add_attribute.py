f5d7d55f9aec5a29ff745da23e8dbe370abcf5df
AgustinGallotti <Agustingallotti23@gmail.com>
2022-05-30 19:53:00 -0700

    finally add dict

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -4,13 +4,13 @@
 
 def add_attribute(obj, name, value):
     """defined my obj"""
-    if hasattr(obj) and not hasattr(obj, att):
+    if hasattr(obj, "__slots__") and not hasattr(obj, name):
         """The method hasattr, return true if an obj has the given named attr
         or false if it does not"""
         raise TypeError("can't add new attribute")
-    if not hasattr(obj) and not hasattr(obj, __slots__):
+    if not hasattr(obj, "__dict__") and not hasattr(obj, "__slots__"):
         """Concept of memory optimisation on objects,
         __slots__, reduce the size of objects"""
         raise TypeError("can't add new attribute")
 
-    setattr(obj, att, value)
+    setattr(obj, name, value)
