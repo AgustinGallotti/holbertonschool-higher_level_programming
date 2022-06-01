a49f2665cd47370f227f8ea204b50e96950ef65c
AgustinGallotti <Agustingallotti23@gmail.com>
2022-05-31 18:09:24 -0700

    from sys

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """list from args and save in a file json format"""
-import sys import json
+from sys import json
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
