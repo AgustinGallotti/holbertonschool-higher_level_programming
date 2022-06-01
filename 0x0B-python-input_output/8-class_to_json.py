545abea9ba88b8abf238c92680cb1efe241afddd
AgustinGallotti <Agustingallotti23@gmail.com>
2022-05-31 18:12:17 -0700

    ___dict__

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,18 +1,7 @@
 #!/usr/bin/python3
+""" funciton that return info of a dictionary"""
 
 
-class Student():
-    """class"""
-
-    def __init__(self, first_name, last_name, age):
-        """define my id"""
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
-
-    def to_json(self, attrs=None):
-        """return info"""
-        if type(attrs) is str:
-            return {key: value for key, value in self__dict__.items() if key in attrs}
-        else:
-            return self.first_name, self.last_name, self.age
+def class_to_json(obj):
+    """def my class and use method dict for info"""
+    return obj.__dict__
