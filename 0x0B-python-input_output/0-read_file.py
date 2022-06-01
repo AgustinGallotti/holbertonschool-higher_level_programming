a080f73122330a3186dccf6d4334c712bf4260fb
AgustinGallotti <Agustingallotti23@gmail.com>
2022-05-31 18:00:01 -0700

    read

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,5 +3,6 @@
 
 
 def read_file(filename=""):
-    with open(filename, "r") as f:
+    """defined my built"""
+    with open(filename, "r", encoding="UTF-8") as f:
         print(f.read(), end="")
