681c19c8ab2b3e29ca884bcc2e26a7c050711672
AgustinGallotti <Agustingallotti23@gmail.com>
2022-05-31 18:06:18 -0700

    add

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,12 +7,16 @@ import json
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
+filename = "add_item.json"
+
 try:
-    listt = load_from_json_file()
+    listt = load_from_json_file(filename)
 except FileNotFoundError:
     listt = []
+
 for arg in sys.argv[1:]:
     """values that are passed during calling of program
     along with thecalling statement"""
-    listt.append(add_item)
+    listt.append(filename)
+
 save_to_json_file(listt, add_item)
