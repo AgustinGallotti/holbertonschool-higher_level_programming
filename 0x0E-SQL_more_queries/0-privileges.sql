84f77a314b8acacd85cbb1a1f5137dc43d399b9d
AgustinGallotti <Agustingallotti23@gmail.com>
2022-07-06 10:01:37 -0700

    tasks, five to repair and quesitons to default

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- sql
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- and the user
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,7 @@
+-- sql
+CREATE TABLE IF NOT EXISTS unique_id (
+  id INT,
+  name VARCHAR(256),
+);
+ALTER TABLE unique_id ALTER id SET DEFAULT 1;
+ADD UNIQUE (id);
