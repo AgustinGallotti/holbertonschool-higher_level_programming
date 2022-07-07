0e357396e2615a28f96f2c7f6e0f591cd319b026
AgustinGallotti <Agustingallotti23@gmail.com>
2022-07-06 17:37:29 -0700

    check iner

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,3 @@
 -- sql
-SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states;
+SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states
 ON cities.state_id=states.id;
