059d85587c8a8aa21c91853c9b1a353f5574987c
AgustinGallotti <Agustingallotti23@gmail.com>
2022-07-06 17:05:34 -0700

    state_id of state

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,11 +1,10 @@
 -- sql
-SELECT
-  id, name
+SELECT id, name
 FROM
   cities
 WHERE
   state_id IN(SELECT id
     FROM
-      state.cities
+      states
     WHERE
-      name = 'California' ORDER BY cities.id);
+      name = 'California' ORDER BY id);
