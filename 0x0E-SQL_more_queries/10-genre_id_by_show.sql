b89010c990d02a89b45519c2191aa8de8111eef2
AgustinGallotti <Agustingallotti23@gmail.com>
2022-07-06 18:00:44 -0700

    join for match

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
 -- sql
-SELECT tv_shows.title, tv_show_genres.genre_id;
-RIGHT JOIN tv_shows_display ON tv_shows.id = tv_show_genres.show_id;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+RIGHT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
