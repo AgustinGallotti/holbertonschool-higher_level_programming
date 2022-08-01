1b9024ae69bd52bc580f9c23eda78cf9e7d84cb8
AgustinGallotti <AgustinGallotti23@gmail.com>
2022-08-01 15:40:26 -0700

    check

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const p = 'process';
+const p = process.argv;
 
-if (process.argv.length <= 2) {
+if (p.length <= 2) {
   console.log('No argument');
-} else if (process.argv.length === 3) {
+} else if (p.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
