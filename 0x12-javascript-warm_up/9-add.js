ec39cbddfba2f3738993e82153cd4f13f89bc3ec
AgustinGallotti <AgustinGallotti23@gmail.com>
2022-08-01 19:34:00 -0700

    checkinnng

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,7 +3,7 @@ const myVar = process.argv;
 function add (a, b) {
   return a + b;
 }
-if(myVar[2] && myVar[3]) {
+if (myVar[2] && myVar[3]) {
   console.log(add(parseInt(myVar[2]), parseInt(myVar[3])));
 } else {
   console.log('NaN');
