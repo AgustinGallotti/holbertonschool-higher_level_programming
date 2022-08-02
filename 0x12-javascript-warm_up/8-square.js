c005a176802b25fa03ecb14b32143bcd8d111347
AgustinGallotti <AgustinGallotti23@gmail.com>
2022-08-01 19:36:50 -0700

    spaces

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,7 +4,7 @@ const myVar = process.argv;
 
 if (parseInt(myVar[2])) {
 	for (let i = 0; i < parseInt(myVar[2]); i++) {
-		console.log('X'.repeat(myVar[2]));
+	  console.log('X'.repeat(myVar[2]));
 	}
 } else {
 	console.log('Missing size');
