d1b89204909fa03b4e7df4ea0bed8fdc02d81635
AgustinGallotti <AgustinGallotti23@gmail.com>
2022-08-01 17:36:20 -0700

    not a nubmer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -5,5 +5,5 @@ const p = process.argv;
 if (parseInt(p[2])) {
   console.log("My number: " + parseInt(p[2]));
 } else {
-  console.log("My number: " + "Not a number");
+  console.log("Not a number");
 }
