e97981468db5acc307be52031f041e5b0d2479a0
AgustinGallotti <AgustinGallotti23@gmail.com>
2022-08-01 19:46:46 -0700

    Checking

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -6,7 +6,6 @@ if (p[2] && p[3]) {
   console.log(p[2] + ' is ' + p[3]);
 } else if (p[2]) {
   console.log(p[2] + ' is ' + 'undefined');
-}
-else {
+} else {
   console.log('undefined' + ' is ' + 'undefined');
 }
