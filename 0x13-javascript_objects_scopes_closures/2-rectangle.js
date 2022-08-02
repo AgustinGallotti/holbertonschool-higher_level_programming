99c0db8360170a6ea0c27ff6953cafebef274aa2
AgustinGallotti <AgustinGallotti23@gmail.com>
2022-08-02 09:32:34 -0700

    check

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -4,8 +4,8 @@ module.exports = class Rectangle {
   constructor (width, height) {
     this.width = width;
     this.height = height;
-    if (height >= 0 || width >= 0) {
-      Object.create();
+    if (height <= 0 || width <= 0) {
+      new Object();
     }
   }
 }
