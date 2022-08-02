3fd976433d14d61654d22308ac4859f9ee04eed9
AgustinGallotti <AgustinGallotti23@gmail.com>
2022-08-02 09:22:26 -0700

    check if stament is true

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (width, height) {
+    this.width = width;
+    this.height = height;
+    if (height >= 0 || width >= 0) {
+      Object.create();
+    }
+  }
+}
