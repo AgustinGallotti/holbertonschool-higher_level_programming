cce8ae0ce0dd239b6aa57d1ac6be98ac0beaa0c7
AgustinGallotti <AgustinGallotti23@gmail.com>
2022-08-02 08:20:45 -0700

    0

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,8 +1,3 @@
 #!/usr/bin/node
 
-class Rectangle {
-  constructor(view = {height: void 0, width: void 0}) {
-    this.height = view.height;
-    this.width = view.width;
-  }
-}
+class Rectangle {}
