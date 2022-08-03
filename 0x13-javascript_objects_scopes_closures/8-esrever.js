18d2503320539fe9eb66ed36f9b16793ee54e1be
AgustinGallotti <AgustinGallotti23@gmail.com>
2022-08-03 06:33:50 -0700

    built-in of reverse

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-exports.esrever = function reve(list) {
+exports.esrever = function (list) {
   var ret = new Array;
   for (var i = list.length-1; i >= 0; i--) {
     ret.push(list[i]);
