734ff42809a9c8e2f29ef3781d72bed61854dbd4
AgustinGallotti <AgustinGallotti23@gmail.com>
2022-08-02 17:38:40 -0700

    probandp

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 
 const Rectangle = require('./4-rectangle');
+
 module.exports = class Square extends Rectangle {
-  constructor(size) {
+  constructor (size) {
     super(size, size);
-    this.name = 'size';
+    this.name = 'Square';
   }
 }
