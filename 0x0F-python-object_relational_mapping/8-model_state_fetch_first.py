87a15d86aa0ae97019cbed86df86c66f13cec21b
AgustinGallotti <Agustingallotti23@gmail.com>
2022-08-07 07:15:48 -0700

    pycode eight

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -11,9 +11,11 @@ from sys import argv
 
 
 if __name__ == '__main__':
-    db = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    db = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                       argv[2], argv[3]), pool_pre_ping=True)
 
     Base.metadata.create_all(db)
+
 
     Session = sessionmaker(bind=db)
     sn = Session()
