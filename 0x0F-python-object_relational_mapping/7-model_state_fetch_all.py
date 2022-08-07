2f1ca3e3fed1ecd415c7d10e40ce9964f3b78c53
AgustinGallotti <Agustingallotti23@gmail.com>
2022-08-06 18:53:00 -0700

    new query, and use pool to prevent errors of connections

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+    module
+"""
+
+
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+from sys import argv
+
+
+if __name__ == '__main__':
+    db = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(db)
+
+    Session = sessionmaker(bind=db)
+    sn = Session()
+    for row in sn.query(State).all():
+        print("{}: '{}'".format(row.id, row.name))
---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.sql
@@ -0,0 +1,3 @@
+-- sql
+-- Insert states
+INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
