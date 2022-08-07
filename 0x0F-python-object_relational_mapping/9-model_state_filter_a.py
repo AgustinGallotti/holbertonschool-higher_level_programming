859a2d283ae31aabda764b25001c1ad67d375b8b
AgustinGallotti <Agustingallotti23@gmail.com>
2022-08-06 19:47:20 -0700

    nine to edit

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+    module
+"""
+
+
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+from sys import argv
+
+
+if __name__ == '__main__':
+    db = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(db)
+
+    Session = sessionmaker(bind=db)
+    sn = Session()
+    row = sn.query(State).first()
+    if row:
+        print("{}: {}".format(row.id, row.name))
