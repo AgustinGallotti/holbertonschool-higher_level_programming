353186b0f37ca47eb5084075d1411ee35030b8b9
AgustinGallotti <AgustinGallotti>
2022-08-19 21:43:06 +0000

    check

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    module
+"""
+
+
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    r = requests.get(sys.argv[1])
+    print(r.headers.get("X-Request-Id"))
