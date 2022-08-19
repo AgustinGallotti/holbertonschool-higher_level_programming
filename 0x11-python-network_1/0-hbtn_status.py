2e0bbe1977f8407ee307d27ba15dff8d3d96886c
AgustinGallotti <AgustinGallotti>
2022-08-19 18:03:11 +0000

    pep

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,7 +5,6 @@
 if __name__ == '__main__':
     import urllib.request
 
-
     url = 'https://intranet.hbtn.io/status'
     status_code = urllib.request.urlopen(url)
     with status_code as response:
