a2007eefc2fc5dbca44d79ebc99f9aba11fd4f0c
AgustinGallotti <AgustinGallotti>
2022-08-19 19:09:18 +0000

    comillas

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -11,4 +11,4 @@ if __name__ == '__main__':
     url = 'https://intranet.hbtn.io'
     req = urllib.request.urlopen(url)
     with req as response:
-        print(response.headers.get("X-Request-Id"))
+        print(response.headers.get('X-Request-Id'))
