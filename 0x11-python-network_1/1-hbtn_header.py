309a43989c8018df4187ccd03f6a10e2827e8f8b
AgustinGallotti <AgustinGallotti>
2022-08-19 19:11:08 +0000

    checking

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     import urllib.request
     import sys
 
-    url = 'https://intranet.hbtn.io'
+    url = sys.argv[1]
     req = urllib.request.urlopen(url)
     with req as response:
         print(response.headers.get('X-Request-Id'))
