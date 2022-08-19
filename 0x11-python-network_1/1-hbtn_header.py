5b6eb1f14579af3bec0338ea9ea73f9f28807c31
AgustinGallotti <AgustinGallotti>
2022-08-19 19:07:22 +0000

    using headers

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    module
+"""
+
+
+if __name__ == '__main__':
+    import urllib.request
+    import sys
+
+    url = 'https://intranet.hbtn.io'
+    req = urllib.request.urlopen(url)
+    with req as response:
+        print(response.headers.get("X-Request-Id"))
