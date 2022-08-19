8e35c8d935b900a9586cee449f3b8478deabb51c
AgustinGallotti <AgustinGallotti>
2022-08-19 18:00:14 +0000

    using prints

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,11 +3,14 @@
 
 
 if __name__ == '__main__':
-	import urllib.request
+    import urllib.request
 
 
-	url = 'https://intranet.hbtn.io/status'
-	status_code = urllib.request.urlopen(url)
-	with status_code as response:
-		st = response.read()
-		print(st)
+    url = 'https://intranet.hbtn.io/status'
+    status_code = urllib.request.urlopen(url)
+    with status_code as response:
+        status_code = response.read()
+        print("Body response:")
+        print("\t- type:", type(status_code))
+        print("\t- content:", status_code)
+        print("\t- utf8 content:", status_code.decode('utf-8'))
