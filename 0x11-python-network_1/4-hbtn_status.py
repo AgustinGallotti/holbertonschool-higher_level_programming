d5aea37f2798a88963a75651c798b81ffb3621c0
AgustinGallotti <AgustinGallotti>
2022-08-19 21:22:31 +0000

    text

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -9,5 +9,5 @@ if __name__ == '__main__':
 
     r = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
-    print("\t- type:", type(r))
-    print("\t- content:", r)
+    print("\t- type:", type(r.text))
+    print("\t- content:", r.text)
