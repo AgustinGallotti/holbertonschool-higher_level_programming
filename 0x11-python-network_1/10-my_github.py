3467cc0bbbd73e7cfdc12f0262f93813bb76612d
AgustinGallotti <AgustinGallotti>
2022-08-20 20:35:43 +0000

    file removed for error

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -4,18 +4,16 @@
 """
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     import requests
     from sys import argv
-    repo = argv[1]
-    owner = argv[2]
-    api = 'https://api.github.com/repos/{}/{}/commits'.format(repo, owner)
-    r = requests.get(api)
-    commits = r.json()
+
+    user = argv[1]
+    passw = argv[2]
+    url = 'https://api.github.com/user'
+    r = requests.get(url, usr=(user, passw))
+    access = r.json()
     try:
-        for i in range(10):
-            print("{}: {}".format(
-                commits[i].get("sha"),
-                commits[i].get("commit").get("author").get("name")))
-    except IndexError:
-        pass
+        print(access['id'])
+    except Exception:
+        print("None")
