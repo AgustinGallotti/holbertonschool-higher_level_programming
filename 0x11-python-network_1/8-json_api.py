8340989c042173fe63048cb7f5552e335c6913a1
AgustinGallotti <AgustinGallotti>
2022-08-20 02:29:50 +0000

    1260

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,9 +6,17 @@
 if __name__ == '__main__':
     import requests
     import sys
-
-    q = {"[id]": sys.argv[1]}
+    if len(sys.argv) > 1:
+        q = {'q': sys.argv[1]}
+    else:
+        q = {'q': ""}
     url = "http://0.0.0.0:5000/search_user"
     r = requests.post(url, q)
-    print(r)
-    print(r.json())
+    try:
+        JS = r.json()
+        if len(JS) == 0:
+            print("No result")
+        else:
+            print("[{}] {}".format(JS["id"], JS["name"]))
+    except Exception:
+        print("Not a valid JSON")
