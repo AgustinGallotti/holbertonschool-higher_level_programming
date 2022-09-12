c9eabf392b9e2af20c00017fc8363872e3a00a81
AgustinGallotti <AgustinGallotti>
2022-09-12 23:51:43 +0000

    check

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = 'https://jsonbin.io/591a64459208345676e6a1ed';
+
+request (url, function (error, response, body) {
+	if (error) {
+		console.log(error);
+	}
+	else {
+		console.log(JSON.parse(body));
+	}
+});
