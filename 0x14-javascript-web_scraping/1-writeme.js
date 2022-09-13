daaeac58fb47def02220cebfe6685ab34d59c57b
AgustinGallotti <AgustinGallotti>
2022-09-13 00:18:47 +0000

    one statment

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,7 +2,6 @@
 
 const fs = require('fs');
 
-fs.writeFile(process.argv[2], process.argv[3], (error, data) => {
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', (error, data) => {
   if (error) console.log(error);
-  else console.log(data);
 });
