f2de2617f1d3982716b9c04fce16d5cca9ca5734
AgustinGallotti <AgustinGallotti>
2022-09-13 00:05:34 +0000

    check

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,8 @@
 #!/usr/bin/node
 
-const request = require('request');
-const url = 'https://jsonbin.io/591a64459208345676e6a1ed';
+let fs = require('fs');
 
-request (url, function (error, response, body) {
-	if (error) {
-		console.log(error);
-	}
-	else {
-		console.log(JSON.parse(body));
-	}
+fs.readFile(process.argv[2], 'utf8', (error, data) => {
+	if (error) console.log(error);
+	else console.log(data);
 });
