c910cd1a777a5b1b06d2c21601f7b4beb312de95
AgustinGallotti <AgustinGallotti>
2022-09-13 01:01:17 +0000

    title with fact

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 const axios = require('axios').default;
-const argv = process.argv
-const url = 'https://swapi-api.hbtn.io/api/films/' + argv[2]
+const argv = process.argv;
+const url = 'https://swapi-api.hbtn.io/api/films/' + argv[2];
 
 axios.get(url).then(function (response) {
   console.log(response.data.title);
